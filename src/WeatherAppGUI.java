2a5eb898e7408f877bfd84c46a13927e5411d386
nefretac <karahanyavuz61@gmail.com>
2024-05-18 01:21:46 +0300

    frontend issues

--- src/WeatherAppGUI.java
+++ src/WeatherAppGUI.java
@@ -1,11 +1,16 @@
+import org.json.simple.JSONObject;
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 public class WeatherAppGUI extends JFrame {
+    private JSONObject weatherData;
     public WeatherAppGUI(){
         //Set up our gui and add a title
         super("Weather App");
@@ -37,13 +42,6 @@ public class WeatherAppGUI extends JFrame {
 
         add(searchTextField);
 
-        //search button
-        JButton searchButton = new JButton(loadImage("src/assets/search.png"));
-
-        //change the cursor to a hand cursor when hovering over this button
-        searchButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-        searchButton.setBounds(375, 13, 47, 45);
-        add(searchButton);
 
         //weather image
         JLabel weatherConditionImage = new JLabel(loadImage("src/assets/cloudy.png"));
@@ -87,6 +85,62 @@ public class WeatherAppGUI extends JFrame {
         windspeedText.setBounds(310,500,85,55);
         windspeedText.setFont(new Font("Dialog", Font.PLAIN,16));
         add(windspeedText);
+
+        //search button
+        JButton searchButton = new JButton(loadImage("src/assets/search.png"));
+
+        //change the cursor to a hand cursor when hovering over this button
+        searchButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        searchButton.setBounds(375, 13, 47, 45);
+        searchButton.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //Get loc from user
+                String userInput = searchTextField.getText();
+
+                //validate input - remove whitespace to ensure non-empty text
+                if (userInput.replaceAll("\\s", "").length() <=0){
+                    return;
+                }
+
+                //retrieve weather data
+                weatherData = WeatherApp.getWeatherData(userInput);
+
+                //update gui
+
+                //update weather image
+                String weatherCondition = (String) weatherData.get("weather_condition");
+
+                //depending on the condition, we will update the weather image that corresponds with the condition
+                switch (weatherCondition) {
+                    case "Clear sky":
+                        weatherCondition.setIcon(loadImage("src/assets/clear.png"));
+                        break;
+                    case "Cloudy":
+                        weatherCondition.setIcon(loadImage("src/assets/cloudy.png"));
+                    case "Foggy":
+                        weatherCondition.setIcon(loadImage("src/assets/cloudy.png"));
+                    case "Drizzle":
+                        weatherCondition.setIcon(loadImage("src/assets/rain.png"));
+                    case "Freezing Drizzle":
+                        weatherCondition.setIcon(loadImage("src/assets/rain.png"));
+                    case "Snow fall":
+                        weatherCondition.setIcon(loadImage("src/assets/snow.png"));
+                    case "Snow Grains":
+                        weatherCondition.setIcon(loadImage("src/assets/snow.png"));
+                    case "Rain showers":
+                        weatherCondition.setIcon(loadImage("src/assets/rain.png"));
+                    case "Snow showers":
+                        weatherCondition.setIcon(loadImage("src/assets/snow.png"));
+                    case "Thunderstorm":
+                        weatherCondition.setIcon(loadImage("src/assets/thunderstorm.png"));
+                    case "Thunderstorm with Hail":
+                        weatherCondition.setIcon(loadImage("src/assets/thunderstorm.png"));
+
+                }
+            }
+        })
+        add(searchButton);
     }
     //used to create images in our gui components
     private ImageIcon loadImage(String resourcePath){
